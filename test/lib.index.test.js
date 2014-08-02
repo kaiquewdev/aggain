e57eb1fdad6ba2fc69886318f5f5215f65417f4e
Kaique da Silva <kaique.developer@gmail.com>
2014-08-02 19:41:46 -0300

    Change the order of tests

--- test/lib.index.test.js
+++ test/lib.index.test.js
@@ -13,6 +13,31 @@ describe('Agni', function () {
     agni.hasDotAgni('./test/case').should.not.be.ok;
   });
 
+  it('should be transform label into a valid pattern', function (done) {
+    agni.format('variable', 'test-case').should.be.eql('testCase');
+    agni.format('variable', 'test_case').should.be.eql('testCase');
+    agni.format('method', 'test-case').should.be.eql('testCaseHandler');
+    agni.format('method', 'test_case').should.be.eql('testCaseHandler');
+    agni.format('class', 'person').should.be.eql('Person');
+    agni.format('class', 'test_case-class').should.be.eql('TestCaseClass');
+    done();
+  });
+
+  it('should be validate a label with dash', function (done) {
+    agni.validate('label', 'test-case').should.be.ok;
+    done();
+  });
+
+  it('should be validate a label with camel case pattern', function (done) {
+    agni.validate('label', 'testCase').should.be.not.ok;
+    done();
+  });
+
+  it('should not be validate a label with space', function (done) {
+    agni.validate('label', 'test case').should.be.not.ok;
+    done();
+  });
+
   it('fire up the egine', function (done) {
     agni.up('./test/case', function(err) {
       should.not.exist(err);
@@ -391,31 +416,6 @@ describe('Agni', function () {
       );
       done();
     });
-  });
-
-  it('should be transform label into a valid pattern', function (done) {
-    agni.format('variable', 'test-case').should.be.eql('testCase');
-    agni.format('variable', 'test_case').should.be.eql('testCase');
-    agni.format('method', 'test-case').should.be.eql('testCaseHandler');
-    agni.format('method', 'test_case').should.be.eql('testCaseHandler');
-    agni.format('class', 'person').should.be.eql('Person');
-    agni.format('class', 'test_case-class').should.be.eql('TestCaseClass');
-    done();
-  });
-
-  it('should be validate a label with dash', function (done) {
-    agni.validate('label', 'test-case').should.be.ok;
-    done();
-  });
-
-  it('should be validate a label with camel case pattern', function (done) {
-    agni.validate('label', 'testCase').should.be.not.ok;
-    done();
-  });
-
-  it('should not be validate a label with space', function (done) {
-    agni.validate('label', 'test case').should.be.not.ok;
-    done();
   });
 
   after(function () {

badceaf48ae529fa44f59a89d7844fcaab07a57f
Kaique da Silva <kaique.developer@gmail.com>
2015-01-02 12:48:59 -0200

    Tests
    refactor was completed

--- tests/start.test.js
+++ tests/start.test.js
@@ -6,17 +6,25 @@ var path = require('path');
 var exec = require('child_process').exec;
 
 describe('start', function () {
-  var dir = path.resolve(process.cwd(), './tests/case');
+  var dir = path.resolve(process.cwd(), './tests/case/new-structure');
   var dotAgniLocation = path.resolve(dir, '.agni');
+
+  global.fs = fs;
+  global.path = path;
+  global.agni = agni;
+  global.dir = dir;
+  global.dotAgniLocation = dotAgniLocation;
 
   it('start up the structure', function (done) {
     agni.start('./tests/case', 'new-structure', function(err) {
       should.not.exist(err);
-      fs.existsSync(path.resolve(dir, 'new-structure')).should.be.ok;
-      fs.existsSync(path.resolve(dir, 'new-structure/.agni')).should.be.ok;
+      fs.existsSync(path.resolve(dir)).should.be.ok;
+      fs.existsSync(path.resolve(dir, '.agni')).should.be.ok;
       done();
     });
   });
+
+  require('./cases');
 
   it('has dot agni location', function () {
     agni.hasDotAgni('./tests/case/new-structure').should.be.ok;
@@ -25,5 +33,6 @@ describe('start', function () {
   after(function () {
     exec('rm -rf ' + dotAgniLocation, console.log.bind(console));
     exec('rm -rf ' + dir + '/*', console.log.bind(console));
+    exec('rm -rf ' + dir, console.log.bind(console));
   });
 });
--- tests/up.test.js
+++ tests/up.test.js
@@ -9,6 +9,12 @@ describe('up', function () {
   var dir = path.resolve(process.cwd(), './tests/case');
   var dotAgniLocation = path.resolve(dir, '.agni');
 
+  global.fs = fs;
+  global.path = path;
+  global.agni = agni;
+  global.dir = dir;
+  global.dotAgniLocation = dotAgniLocation;
+
   it('fire up the egine', function (done) {
     agni.up('./tests/case', function(err) {
       should.not.exist(err);
@@ -20,6 +26,16 @@ describe('up', function () {
 
   it('has dot agni location', function () {
     agni.hasDotAgni('./tests/case').should.be.ok;
+  });
+
+  require('./cases');
+
+  it('fail to create dot agni dir in an existing context with up', function (done) {
+    agni.up('./tests/case', function (err) {
+      fs.existsSync(dotAgniLocation).should.be.ok;
+      err.should.be.eql('.agni exists in the ' + dotAgniLocation.replace('/.agni', ''));
+      done();
+    });
   });
 
   after(function () {

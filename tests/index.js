bb046c1b0b06a717e3c96c2030ab3de5869692b2
Kaique da Silva <kaique.developer@gmail.com>
2017-02-23 18:38:34 -0300

    Reduced the tests

--- tests/index.js
+++ tests/index.js
@@ -2,5 +2,5 @@
 exports.hasDotAgniLocation = require('./has-dot-agni-location.test');
 exports.format = require('./format.test');
 exports.validate = require('./validate.test');
-exports.up = require('./up.test');
-exports.start = require('./start.test');
+//exports.up = require('./up.test');
+//exports.start = require('./start.test');
